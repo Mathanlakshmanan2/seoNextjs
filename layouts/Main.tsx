dac14d72e5e6b2ba48dfc93d2d221caab2848fe1
Sharmilee S <sharmilee.s@tarnea.com>
2023-05-16 16:24:07 +0530

    updated google

--- layouts/Main.tsx
+++ layouts/Main.tsx
@@ -15,6 +15,8 @@ export default ({ children, title = 'Next.js Ecommerce' }: LayoutType) => {
     <div className="app-main">
       <Head>
         <meta charSet="utf-8" />
+        <meta name="google-site-verification" content="zPnMGLNYLN_89V82xeiJFDE--NWuF5B0ponVI_iguKA" />
+        <meta name="msvalidate.01" content="3846A2694A3BBD31ACD111EC5E58A93E" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <title>{ title }</title>
         <meta name="description" content="Get powerful prescription medications from our online store - all without leaving your home! All of our medications come with a 100% satisfaction guarantee."/>
@@ -22,8 +24,7 @@ export default ({ children, title = 'Next.js Ecommerce' }: LayoutType) => {
         <meta property="og:description" content="Find the best drugs, vitamins, and supplements at the best prices in your area with our online store"/>
         <meta property="og:url" content="https://snipcart.com/"/>
         <meta property="og:type" content="website"/>
-        <meta name="google-site-verification" content="zPnMGLNYLN_89V82xeiJFDE--NWuF5B0ponVI_iguKA" />
-        <meta name="msvalidate.01" content="3846A2694A3BBD31ACD111EC5E58A93E" />
+        
       </Head>
 
       <Header />
